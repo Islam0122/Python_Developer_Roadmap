d2407f29d102846ae231259e84d082c7ceb85d53
islam <dujsobaevislam01@gmail.com>
2023-12-31 00:34:21 +0600

    Lists, Tuples, Sets, Dictionaries

---
+++ Learn the Basics/Lists, Tuples, Sets, Dictionaries/code.py
@@ -0,0 +1,20 @@
+def print_elements(elements):
+    for element in elements:
+        print(element)
+
+
+# Списки
+my_list = [1, 2, 3, 4, 5]
+print_elements(my_list)
+
+# Кортежи
+my_tuple = (1, 2, 3, 4, 5)
+print_elements(my_tuple)
+
+# Множества
+my_set = {1, 2, 3, 4, 5}
+print_elements(my_set)
+
+# Словари
+my_dict = {'one': 1, 'two': 2, 'three': 3}
+print_elements(my_dict.keys())
